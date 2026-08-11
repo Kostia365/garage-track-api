df4612abf50837e19b31f88acd22dbf08eebb5ac
Kostia365 <90925004+Kostia365@users.noreply.github.com>
2026-08-11 15:34:25 +0200

    init: start project

---
+++ src/main/java/dev/liauchuk/garagetrack/GarageTrackApiApplication.java
@@ -0,0 +1,13 @@
+package dev.liauchuk.garagetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageTrackApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GarageTrackApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/liauchuk/garagetrack/GarageTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.liauchuk.garagetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageTrackApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
